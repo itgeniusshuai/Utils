de6f9201447fce2a23c3bf597e4ae068e2b1b66e
zhaoshuai3 <zhaoshuai3@xin.com>
2017-08-03 16:15:00 +0800

    change days between

--- Utils/src/main/java/com/utils/DateUtils.java
+++ Utils/src/main/java/com/utils/DateUtils.java
@@ -11,11 +11,22 @@ public class DateUtils {
 	 * @return
 	 */
 	public static int daysBetween(Date firstDate, Date secondDate) {
-		long firstTime = firstDate.getTime();
-		long secondTime = secondDate.getTime();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(firstDate);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.HOUR_OF_DAY,0);
+		long firstTime = calendar.getTimeInMillis();
+		calendar.setTime(secondDate);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.HOUR_OF_DAY,0);
+		long secondTime = calendar.getTimeInMillis();
 		long time = firstTime - secondTime;
 		int days = (int) (time/1000/60/60/24);
-		return days;
+		return days<0?-days:days;
 	}
 	/**
 	 * 获取某天的天末尾的时间
